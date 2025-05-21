76c655147bb7f607daeeb7d2b802befdafd43254
Andre Terron <andre@statsig.com>
2025-05-21 14:50:45 -0700

    Initial Commit

---
+++ RenameSwiftTest/RenameSwiftTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  RenameSwiftTestApp.swift
+//  RenameSwiftTest
+//
+//  Created by Andre Terron on 5/21/25.
+//
+
+import SwiftUI
+
+@main
+struct RenameSwiftTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
